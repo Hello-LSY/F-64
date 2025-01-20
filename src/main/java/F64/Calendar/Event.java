7b1bb10e834f28dd8b70b928e8a02121bad09d4e
Hello-LSY <leesy9520@naver.com>
2025-01-20 22:12:00 +0900

    refactor: Data 어노테이션 제거

--- src/main/java/F64/Calendar/Event.java
+++ src/main/java/F64/Calendar/Event.java
@@ -1,23 +1,23 @@
 package F64.Calendar;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
-
     private LocalDateTime startDate;
-
     private LocalDateTime endDate;
-
     private boolean allDay;
 }
